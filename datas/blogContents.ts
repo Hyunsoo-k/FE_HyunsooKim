29295aa4176eb09ffa12d56c1d2c8494d429509a
Hyunsoo_kim <red_panda119@naver.com>
2024-05-06 18:08:39 +0900

    feat: BlogContents 데이터 추가

---
+++ datas/blogContents.ts
@@ -0,0 +1,82 @@
+const contentData = [
+  {
+    thumbnail: "/images/blog-thumbnail/fetch.png",
+    title: "fetch",
+    content: "네트워크 요청을 보내거나, 응답을 처리하는 웹 API",
+    url: "https://dog-foot.tistory.com/12"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/react-hook-form.png",
+    title: "react-hook-form",
+    content: "폼의 유효성을 관리하는 라이브러리",
+    url: "https://dog-foot.tistory.com/37"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/promise.png",
+    title: "Promise 객체",
+    content: "비동기 작업을 처리할 때 사용되는 객체",
+    url: "https://dog-foot.tistory.com/65"
+  },
+  {
+    thumbnail: "/images/skill-thumbnail/axios.png",
+    title: "axios",
+    content: "Promise 기반의 HTTP 클라이언트 라이브러리",
+    url: "https://dog-foot.tistory.com/44"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/grid.png",
+    title: "grid",
+    content: "요소를 2차원 배치 시킬 수 있는 CSS 모듈",
+    url: "https://dog-foot.tistory.com/86"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/tanstack.png",
+    title: "tanstack query",
+    content: "서버 상태를 관리하는 라이브러리",
+    url: "https://dog-foot.tistory.com/66"
+  },
+  {
+    thumbnail: "/images/skill-thumbnail/react-router-dom.png",
+    title: "react-router-dom",
+    content: "현재 경로와 일치하는 컴포넌트를 렌더링 시켜주는 라이브러리",
+    url: "https://dog-foot.tistory.com/39"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/context.png",
+    title: "context",
+    content: "prop drilling을 방지하기 위한 훅",
+    url: "https://dog-foot.tistory.com/33"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/Cascading,position.png",
+    title: "Cascading, position",
+    content: "스타일이 요소에 적용되는 방식, 요소의 위치를 지정",
+    url: "https://dog-foot.tistory.com/14"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/async,await.png",
+    title: "async/await",
+    content: "Promise 객체를 다루는 비동기 문법",
+    url: "https://dog-foot.tistory.com/59"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/intersectionObserver.png",
+    title: "intersectionObserver",
+    content: "요소와 뷰포트의 관계 상태를 감지하는 객체를 생성하는 API",
+    url: "https://dog-foot.tistory.com/88"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/createPortalTrouble.png",
+    title: "createPortal 이슈",
+    content: "createPortal로 모달을 띄웠을 때, 백 드롭을 포함한 요소가 현재 레이아웃과 맞지 않아 가로스크롤이 생김",
+    url: "https://dog-foot.tistory.com/68"
+  },
+  {
+    thumbnail: "/images/blog-thumbnail/event.png",
+    title: "Event",
+    content: "프로그래밍하고 있는 시스템에서 일어나는 사건(action) 혹은 발생(occurrence)",
+    url: "https://dog-foot.tistory.com/10"
+  },
+];
+
+export { contentData };
