553a09b973d72651bbc6c7fce8757e5c1d75f36c
Hyunsoo_kim <red_panda119@naver.com>
2024-05-06 17:13:23 +0900

    Feat: 메인 페이지 state명 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { Article } from "@/types/main";
+import Header from "@/components/header";
 import Home from "@/components/home";
 import styles from "@/styles/Main.module.css";
 
@@ -9,6 +10,7 @@ const Main = () => {
 
   return (
     <div className={styles["wrapper"]}>
+      <Header setArticle={setArticle} />
       <div className={styles["section"]}>
         <div className={styles["section__mainImg"]}></div>
         <div className={styles["section__content"]}>
