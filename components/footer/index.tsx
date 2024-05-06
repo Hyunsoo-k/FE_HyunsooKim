912457ce47bd82f1b7fe2367f881031f117b32b5
Hyunsoo_kim <red_panda119@naver.com>
2024-05-07 06:47:00 +0900

    Ui: Footer 컴포넌트 Ui 구현

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,13 @@
 import styles from "./index.module.css";
 
 const Footer = () => {
-  return(<div className={styles["wrapper"]}></div>)
-}
+  return (
+    <div className={styles["wrapper"]}>
+      <p className={styles["text"]}>
+        CopyRight 2024<span>_Hyunsoo kim</span>
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
